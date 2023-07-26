c6f22d18f35791d95c0e343bb4a1edf2143dbe28
preethipantangi <pantangisaipreethi@gmail.com>
2023-07-26 18:49:15 -0400

    Modified the code structure and added capability to add a new comment and updated readme

--- src/components/NestedComments/NestedComments.js
+++ src/components/NestedComments/NestedComments.js
@@ -3,45 +3,6 @@ import DisplayComment from './DisplayComment';
 
 const NestedComments = () => {
 
-
-  /**
-   * Structure of the nested comments
-   * {
-    id: 1,
-    comment: 'Hello everyone. This is a nested comments implementation.',
-    replies: [
-      {id: 2,
-        comment: 'This is reply 1',
-        replies: {
-          id: 3, 
-          comment: 'This is reply 1 for reply 1'
-        }
-      },
-      {id: 3,
-        comment: 'This is reply 2',
-        replies: {
-          id: 4, 
-          comment: 'This is reply 1 for reply 2'
-        }
-      }
-    ]
-  },
-  {
-    id: 5,
-    comment: "Happy New Year to everyone. What are your resolutions for this year?",
-    replies: [
-      {
-        id: 6,
-        comment: "This is reply 1 for comment 2"
-      },
-      {
-        id: 7,
-        comment: "This is reply 2 for comment 2"
-      }
-    ]
-  }
-   */
-
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState('');
 
